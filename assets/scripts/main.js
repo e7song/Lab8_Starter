65568546adfbec36095b04bec05c2cd06ed6028d
Eric Song <e7song@ucsd.edu>
2025-05-27 05:13:29 -0700

    testing the final deployment? oddities happening SW doesnt work

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -53,7 +53,7 @@ function initializeServiceWorker() {
       //            "Using Service Workers" will help you here)
       try {
         const regi = await navigator.serviceWorker.register("./sw.js", {
-          scope: "/",
+          scope: "./",
         });
         // B4. TODO - Once the service worker has been successfully registered, console
         //            log that it was successful.
